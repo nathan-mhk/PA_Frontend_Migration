e4da6214d8a6d8c75f51749e1d742d14fe428cc6
OojivsoO <49951479+OojivsoO@users.noreply.github.com>
2020-06-17 15:04:15 +0800

    UI for demo

--- src/plugins/DefectReport/DefectReport.js
+++ src/plugins/DefectReport/DefectReport.js
@@ -1,17 +1,33 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import classnames from 'classnames';
 import buttonImg from './button.png';
+import buttonGo from './button_go.png';
 import style from './DefectReport.module.css';
 
+class DefectReport extends Component {
+  componentDidUpdate(prevProps) {
+    const { x, y, movingX, movingY, floor, enhanceMapItemsHandler } = this.props;
+    const { success, mapItems } = this.props.mapItemStore;
 
-class DefectReport extends Component{
+    if ((prevProps && !prevProps.mapItemStore.loading) || !success) {
+      return;
+    }
+
+    const reportId = 'Defect Report';
+
+    enhanceMapItemsHandler([
+      {
+        id: reportId,
+        others: { xLoc: x, yLoc: y, xMoving: movingX, yMoving: movingY, locFloor: floor, state:true},
+      },
+    ]);
+
+    console.log(x+' '+y);
+  }
 
   componentWillUnmount() {
     const { clearPluginMapItemsHandler } = this.props;
     clearPluginMapItemsHandler();
-  }
-
-  componentDidUpdate(prevProps) {
   }
 
   render() {
@@ -28,42 +44,74 @@ class DefectReport extends Component{
           type="button"
           onClick={() => openOverlayHandler('Defect Report')}
         >
-          <img className={buttonClassName} src={buttonImg} alt='Defect Report'/>
+          <img className={buttonClassName} src={buttonImg} alt="Defect Report" />
         </button>
       </div>
-    )
+    );
   }
-
 }
 
-function ReportUI({x,y,moveingX,movingY,floor,name}){
-  // if(name!=='Report Defect')return null;
-  // return <h1>OUO</h1>
-  return null;
+function ReportUI({name,others}) {
+  if(name!=='Defect Report')
+    return null;
+  let x = others.xLoc, y = others.yLoc,
+    movingX = others.xMoving, movingY = others.yMoving,
+    floor = others.locFloor;
+
+  return (
+    <div>
+      <div style={{marginTop: "20px"}}>Defect Location:  </div>
+      <div style={{marginTop: "20px"}}>
+        <table>
+          <tr>
+            <th>
+              <input type="text"></input>
+            </th>
+            <th>
+              <button
+                className={style.button}
+                type="button"
+              >
+                <img className={style.goButtonImage} src={buttonGo} alt="GO button" />
+              </button>
+            </th>
+          </tr>
+        </table>
+      </div>
+      <div className={style.topMagrin}>
+      <button
+          className={style.button}
+          type="button"
+         onClick={()=>window.open('https://maximo.ust.hk/maximo','_blank')}
+        ><img className={style.buttonImage} src={buttonImg} alt="Defect Report" /></button>
+      </div>
+    </div>
+
+  );
 }
-
-
 
 const defaultOff = false;
 const name = 'Defect Report';
 const core = false;
 
-
 const MapCanvasPlugin = {
   Component: DefectReport,
-  connect: ['x', 'y', 'movingX', 'movingY', 'floor', 'platform', 'openOverlayHandler', 'clearPluginMapItemsHandler',]
-}
+  connect: [
+    'x',
+    'y',
+    'movingX',
+    'movingY',
+    'floor',
+    'platform',
+    'openOverlayHandler',
+    'clearPluginMapItemsHandler',
+    'enhanceMapItemsHandler',
+    'mapItemStore',
+  ],
+};
 
 const OverlayContentPlugin = {
   Component: ReportUI,
-}
-
-export{
-  name,
-  defaultOff,
-  core,
-  MapCanvasPlugin,
-  OverlayContentPlugin,
 };
 
-
+export { name, defaultOff, core, MapCanvasPlugin, OverlayContentPlugin };
