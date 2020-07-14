ada9601e823e047c9a769660ea7f64008efd2f7e
Nathan <mhkongaa@connect.ust.hk>
2020-07-14 19:07:09 +0800

    Stash for migration

--- src/plugins/DefectReport/DefectReport.js
+++ src/plugins/DefectReport/DefectReport.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
 import buttonImg from './button.png';
-import buttonGo from './button_go.png';
 import style from './DefectReport.module.css';
 import FloorPrimaryPanelView from "../../components/FloorPrimaryPanelView/FloorPrimaryPanelView";
 import Floor from "../../components/Floor/Floor";
@@ -127,24 +126,6 @@ class ReportUI extends Component {
             FloorView={FloorPrimaryPanelView}
           />
         </div>
-        {/*<div style={{marginTop: "20px"}}>Defect Location:</div>*/}
-        {/*<div style={{marginTop: "20px"}}>*/}
-        {/*  <table>*/}
-        {/*    <tr>*/}
-        {/*      <th>*/}
-        {/*        <input type="text"></input>*/}
-        {/*      </th>*/}
-        {/*      <th>*/}
-        {/*        <button*/}
-        {/*          className={style.button}*/}
-        {/*          type="button"*/}
-        {/*        >*/}
-        {/*          <img className={style.goButtonImage} src={buttonGo} alt="GO button"/>*/}
-        {/*        </button>*/}
-        {/*      </th>*/}
-        {/*    </tr>*/}
-        {/*  </table>*/}
-        {/*</div>*/}
         <div className={style.topMagrin}>
           <button
             className={style.button}
